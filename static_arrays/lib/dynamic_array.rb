de95e6e58ee6a43b1edc21f7abaeac37157cf439
Kevin <kevinmoore9@users.noreply.github.com>
2017-02-03 09:21:52 -0800

    add static arrays

---
+++ static_arrays/lib/dynamic_array.rb
@@ -0,0 +1,68 @@
+require_relative "static_array"
+
+class DynamicArray
+  attr_reader :length
+
+  def initialize
+    self.store = StaticArray.new(8)
+    self.capacity = 8
+    self.length = 0
+  end
+
+  # O(1)
+  def [](index)
+    check_index(index)
+    store[index]
+  end
+
+  # O(1)
+  def []=(index, value)
+    check_index(index)
+    store[index] = value
+  end
+
+  # O(1)
+  def pop
+    popped = self[length - 1]
+    self[length - 1] = nil
+    self.length -= 1
+
+    popped
+  end
+
+  # O(1) ammortized; O(n) worst case. Variable because of the possible
+  # resize.
+  def push(val)
+    resize! if length == capacity
+    self.length += 1
+    self[length - 1] = val
+
+    nil
+  end
+
+  # O(n): has to shift over all the elements.
+  def shift
+  end
+
+  # O(n): has to shift over all the elements.
+  def unshift(val)
+  end
+
+  protected
+  attr_accessor :capacity, :store
+  attr_writer :length
+
+  def check_index(index)
+  end
+
+  # O(n): has to copy over all the elements to the new store.
+  def resize!
+    new_cap = capacity * 2
+    new_store = StaticArray.new(new_cap)
+    [0...length].each do |i|
+      new_store[i] = self[i]
+    end
+    self.capacity = new_cap
+    self.store = new_store
+  end
+end
